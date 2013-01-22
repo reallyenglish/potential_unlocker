d172d053f31ce834f5317750834b7a13e6020916
Stephen Bartholomew <stephenb@reallyenglish.com>
2013-01-22 09:36:29 +0000

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PotentialUnlocker::Application.config.secret_token = '7059e9e6492804ab8c5a975c412f59fa7df206229ff6f04964aec62f08ee585e936b59107fdd1d8f1a6d194a48f821b68e5852242cb7bbb7dca80120a0b1bb54'
